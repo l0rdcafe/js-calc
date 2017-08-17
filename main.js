e3fa799f2be030d018007b2713fe3647a387bc73
Ismail Arafa <ismailarafa18@gmail.com>
2017-08-17 12:13:08 -0400

    add functionality

--- main.js
+++ main.js
@@ -5,36 +5,36 @@ var inputScreen = document.getElementById('screen');
 
 model.equation = {
   solved: false,
-  left: '',
-  right: '',
+  left: 0,
+  right: 0,
   operand: ''
 };
 
 model.add = function (a, b) {
   var result = parseFloat(a, 10) + parseFloat(b, 10);
-  inputScreen.innerHTML = result;
+  return result;
 };
 
 model.subtract = function (a, b) {
   var result = parseFloat(a, 10) - parseFloat(b, 10);
-  inputScreen.innerHTML = result;
+  return result;
 };
 
 model.multiply = function (a, b) {
   var result = parseFloat(a, 10) * parseFloat(b, 10);
-  inputScreen.innerHTML = result;
+  return result;
 };
 
 model.divide = function (a, b) {
   var result = parseFloat(a, 10) / parseFloat(b, 10);
-  inputScreen.innerHTML = result;
+  return result;
 };
 
 model.factorial = function (n) {
   if (n === 0) {
     return 1;
   }
-  return n * model.factorial(n);
+  return n * model.factorial(n - 1);
 };
 
 handlers.keyHandle = function (elmCont) {
@@ -43,36 +43,48 @@ handlers.keyHandle = function (elmCont) {
   }
   if (model.equation.operand !== '') {
     model.equation.right += elmCont;
-    inputScreen.innerHTML += elmCont;
   } else {
     model.equation.left += elmCont;
-    inputScreen.innerHTML += elmCont;
   }
+  inputScreen.innerHTML += elmCont;
 };
 
 handlers.operandHandle = function (elmCont) {
-  inputScreen.innerHTML += ' ' + elmCont + ' ';
+  model.equation.operand = '';
+  if (elmCont === '!') {
+    inputScreen.innerHTML = '!' + inputScreen.innerHTML;
+  } else {
+    inputScreen.innerHTML += ' ' + elmCont + ' ';
+  }
   model.equation.operand += elmCont;
 };
 
 handlers.clearScreen = function () {
   inputScreen.innerHTML = 0;
-  model.equation.left = '';
-  model.equation.right = '';
+  model.equation.left = 0;
+  model.equation.right = 0;
   model.equation.operand = '';
   model.equation.solved = false;
 };
 
 handlers.solveEqu = function () {
+  var result;
   if (model.equation.operand === '+') {
-    model.add(model.equation.left, model.equation.right);
+    result = model.add(model.equation.left, model.equation.right);
   } else if (model.equation.operand === '-') {
-    model.subtract(model.equation.left, model.equation.right);
+    result = model.subtract(model.equation.left, model.equation.right);
   } else if (model.equation.operand === '*') {
-    model.multiply(model.equation.left, model.equation.right);
+    result = model.multiply(model.equation.left, model.equation.right);
   } else if (model.equation.operand === '/') {
-    model.divide(model.equation.operand, model.equation.right);
+    result = model.divide(model.equation.left, model.equation.right);
+  } else if (model.equation.operand === '!') {
+    result = model.factorial(model.equation.left);
+  } else if (model.equation.left !== '') {
+    result = model.equation.left;
+  } else {
+    result = 0;
   }
+  inputScreen.innerHTML = result;
   model.equation.solved = true;
 };
 
@@ -80,8 +92,16 @@ view.setUpEvents = function () {
   var keypad = document.getElementById('keypad');
   keypad.addEventListener('click', function (event) {
     var clickedElm = event.target;
-    if (model.equation.solved) {
+    var operands = ['add', 'sbtrct', 'mltply', 'divide', 'fctoril'];
+    if (model.equation.solved && operands.indexOf(clickedElm.id) !== -1) {
+      model.equation.left = inputScreen.innerHTML;
+      model.equation.right = 0;
+      model.equation.solved = false;
+      handlers.operandHandle(clickedElm.innerHTML);
+    } else if (model.equation.solved) {
       handlers.clearScreen();
+    } else if (operands.indexOf(clickedElm.id) !== -1) {
+      handlers.operandHandle(clickedElm.innerHTML);
     }
     if (/\d/.test(parseInt(clickedElm.innerHTML, 10)) || clickedElm.id === 'dot') {
       handlers.keyHandle(clickedElm.innerHTML);
@@ -89,8 +109,6 @@ view.setUpEvents = function () {
       handlers.clearScreen();
     } else if (clickedElm.id === 'equal') {
       handlers.solveEqu();
-    } else if (clickedElm.id === 'add' || 'sbtrct' || 'mltply' || 'divide') {
-      handlers.operandHandle(clickedElm.innerHTML);
     }
   });
 };
